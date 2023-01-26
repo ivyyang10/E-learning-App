798162b9b534b042c817cf64f8841f2901d9cb89
Kelvin <kelvin.telon@techelevator.com>
2023-01-26 09:32:55 -0500

    db name change

--- capstone/java/database/create.sh
+++ capstone/java/database/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PGPASSWORD='postgres1'
 BASEDIR=$(dirname $0)
-DATABASE=final_capstone
+DATABASE=elearning_portal_db
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
