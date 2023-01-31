3013025a0ca871237748bd4812e29e4822bc639c
Brian Beam <beam1249@me.com>
2023-01-31 10:22:51 -0500

    finished shell database script

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash, name, email, role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'bob', 'bob@gmail.com','ROLE_USER');
+INSERT INTO users (username,password_hash, name, email, role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'sue', 'sue@gmail.com','ROLE_ADMIN');
 
 COMMIT TRANSACTION;
--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users_course, users, course, curricula;
+DROP TABLE IF EXISTS users_course, users, course, curricula, quiz, question, quiz_question, answer CASCADE;
 
 DROP SEQUENCE IF EXISTS seq_course_id, seq_curricula_id;
 
@@ -8,9 +8,9 @@ CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
-	is_teacher boolean NOT NULL,
 	name varchar(100) NOT NULL,
 	email varchar(50) NOT NULL,
+	role varchar(20) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
@@ -55,6 +55,82 @@ CREATE TABLE users_course (
 	CONSTRAINT FK_users_course_user FOREIGN KEY (user_id) REFERENCES users (user_id),
 	CONSTRAINT FK_users_course_course FOREIGN KEY (course_id) REFERENCES course (course_id)
 );
+
+
+CREATE TABLE quiz (
+    quiz_id serial,
+    quiz_name varchar(20) NOT NULL,
+    course_id int,
+    CONSTRAINT PK_quiz PRIMARY KEY(quiz_id),
+    CONSTRAINT FK_course_id FOREIGN KEY (course_id) REFERENCES course (course_id)
+);
+
+CREATE TABLE question (
+    question_id serial,
+    question_text varchar(100) NOT NULL,
+    correct_answer int NOT NULL,
+    CONSTRAINT PK_question PRIMARY KEY(question_id)
+);
+
+CREATE TABLE quiz_question (
+    quiz_id int,
+    question_id int,
+    question_number int NOT NULL,
+    CONSTRAINT PK_quiz_question PRIMARY KEY(quiz_id, question_id),
+	CONSTRAINT FK_quiz_id FOREIGN KEY(quiz_id) REFERENCES quiz(quiz_id),
+	CONSTRAINT FK_question_id FOREIGN KEY(question_id) REFERENCES question(question_id)
+);
+
+CREATE TABLE answer (
+    question_id int,
+    answer_number int,
+    answer_text varchar(50) NOT NULL,
+    CONSTRAINT PK_answer PRIMARY KEY(question_id, answer_number),
+	CONSTRAINT FK_answer_question FOREIGN KEY (question_id) REFERENCES question(question_id)
+);
+
+INSERT INTO quiz
+(quiz_name)
+VALUES
+('test_quiz');
+
+--Question 1
+INSERT INTO question
+(question_text, correct_answer)
+VALUES
+('What color is the sky?', 2);
+
+INSERT INTO answer
+(question_id, answer_number, answer_text)
+VALUES
+(lastval(), 1, 'yellow'),
+(lastval(), 2, 'blue'),
+(lastval(), 3, 'green'),
+(lastval(), 4, 'red');
+
+INSERT INTO quiz_question
+(quiz_id, question_id, question_number)
+VALUES
+((SELECT quiz_id FROM quiz WHERE quiz_name = 'test_quiz'), lastval(), 1);
+
+--Question 2
+INSERT INTO question
+(question_text, correct_answer)
+VALUES
+('A skeleton walks into a bar, and says to the bartender, "Give me a beer and a ..."', 3);
+
+INSERT INTO answer
+(question_id, answer_number, answer_text)
+VALUES
+(lastval(), 1, 'hair brush'),
+(lastval(), 2, 'dry erase marker'),
+(lastval(), 3, 'mop'),
+(lastval(), 4, 'yo-yo');
+
+INSERT INTO quiz_question
+(quiz_id, question_id, question_number)
+VALUES
+((SELECT quiz_id FROM quiz WHERE quiz_name = 'test_quiz'), lastval(), 2);
 
 
 COMMIT TRANSACTION;
