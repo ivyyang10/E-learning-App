5f863f0a9aaed06bbc72bd7bf2ed4a0a7c48d74d
Austin Smith <acsmithwt@gmail.com>
2023-02-05 16:07:32 -0500

    Fixed course view / added some CSS

--- capstone/vue/src/components/TeacherCourseArea.vue
+++ capstone/vue/src/components/TeacherCourseArea.vue
@@ -1,10 +1,24 @@
 <template>
-  <div id="teacherCenterView">
-    <p v-bind:to="{ name: 'courses', params: { id: course.courseId } }">
-      {{ course.name }}
-      {{ course.dailyInstruction }}
-      {{ course.links }}
-    </p>
+  <div id="teacher-center-view">
+    <div id="working-area-header">
+      <h1 v-bind:to="{ name: 'courses', params: { id: course.courseId } }">
+        {{ course.courseName }}
+      </h1>
+    </div>
+    <div id="row-two">
+      <label id="instruction-label" for="instruction">Daily Instruction</label>
+
+      <p id="instruction">
+        {{ course.dailyInstruction }}
+      </p>
+
+      <label id="resources-label" for="resources">Class Resources</label>
+
+      <p id="resources">{{ course.links }}</p>
+      <router-link v-bind:to="{ name: 'quizview' }"
+          ><button id="create-quiz-button">Create Quiz</button></router-link
+        >
+    </div>
   </div>
 </template>
 
@@ -35,4 +49,34 @@ export default {
 </script>
 
 <style>
+#teacher-center-view {
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-rows: 1fr 1fr;
+  grid-template-areas:
+    "working-area-header"
+    "row-two";
+}
+#working-area-header {
+  grid-area: working-area-header;
+}
+#row-two {
+  grid-area: row-two;
+  padding: 10px
+}
+#instruction {
+  padding: 25px;
+  border: solid 2px;
+  border-color: #75b9be;
+  border-radius: 25px;
+}
+#resources {
+  padding: 25px;
+  border: solid 2px;
+  border-color: #75b9be;
+  border-radius: 25px;
+}
+#create-quiz-button {
+  padding: 10px;
+}
 </style>
--- capstone/vue/src/views/StudentCourseDetail.vue
+++ capstone/vue/src/views/StudentCourseDetail.vue
@@ -1,8 +1,17 @@
 <template>
 
     <div id="student-working-area">  
-      <student-course-area id="student-course-area" />
-      <student-right-side-bar id="student-side-bar" />
+
+      <div id="course-show">
+        <student-course-area id="student-course-area" v-show="$store.state.user.authorities[0].name === 'ROLE_USER'"/>
+        <teacher-course-area id="teacher-course-area" v-show="$store.state.user.authorities[0].name === 'ROLE_ADMIN'"/>
+      </div>
+
+      <div id="side-bar-show">
+        <student-right-side-bar id="student-side-bar" v-show="$store.state.user.authorities[0].name === 'ROLE_USER'"/>
+        <teacher-right-side-bar id="teacher-side-bar" v-show="$store.state.user.authorities[0].name === 'ROLE_ADMIN'"/>
+      </div>
+      
     </div>
 
 </template>
@@ -10,11 +19,15 @@
 <script>
 import StudentRightSideBar from "../components/StudentRightSideBar.vue";
 import StudentCourseArea from "../components/StudentCourseArea.vue";
+import TeacherCourseArea from '../components/TeacherCourseArea.vue';
+import TeacherRightSideBar from '../components/TeacherRightSideBar.vue'
 
 export default {
   components: {
     StudentRightSideBar,
     StudentCourseArea,
+    TeacherCourseArea,
+    TeacherRightSideBar
   },
 };
 </script>
@@ -26,6 +39,12 @@ export default {
   grid-template-areas: 
   "student-course-area student-side-bar";
 }
+#teacher-working-area {
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  grid-template-areas: 
+  "teacher-course-area teacher-side-bar";
+}
 #class-title-header {
   grid-area: class-title-header;
 }
@@ -35,11 +54,21 @@ export default {
 #student-course-area {
     border-right: solid;
     padding-right: 15px;
-  border-color: black;
+  border-color: #75B9BE;
   grid-area: student-course-area;
 }
 #student-side-bar {
   grid-area: student-side-bar;
+  padding: 10px;
+}
+#teacher-course-area {
+  border-right: solid;
+  padding-right: 15px;
+  border-color: #75B9BE;
+  grid-area: teacher-course-area;
+}
+#teacher-side-bar {
+  grid-area: teacher-side-bar;
   padding: 10px;
 }
 </style>
