2b46bd757f614d11bd3e182eab66ac87edc15320
Xiaojie <ivyyangxiaoxiao@gmail.com>
2023-02-06 16:00:16 -0500

    homework submission

---
+++ capstone/java/src/main/java/com/techelevator/dao/HomeworkDao.java
@@ -0,0 +1,9 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Homework;
+
+public interface HomeworkDao {
+    Homework findHomeworkById(int HomeworkId);
+
+    Homework submitHomework(int courseId, int studentId, String hwSubmission);
+}
