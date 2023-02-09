276ce65202e2029d9ccee617e8e50308cc0832dd
Xiaojie <ivyyangxiaoxiao@gmail.com>
2023-02-08 20:55:17 -0500

    small change

--- capstone/vue/src/components/StudentProgress.vue
+++ capstone/vue/src/components/StudentProgress.vue
@@ -19,13 +19,15 @@ export default {
   computed: {
     calculateProgress() {
       let score = 0;
+      let length=this.progressArr.length;
       if (this.progressArr.length === 0) {
         return 0 + "%";
       }
-      for (let i = 0; i < this.progressArr.length; i++) {
-        if (this.progressArr[i].completed == true) {
-          score++;
+      for (let i = 0; i < this.progressArr.length -1; i++) {
+        if(this.progressArr[i].grade === 0){
+         length=length-1;
         }
+        score = this.progressArr[i].grade + score;   
       }
       return (score / this.progressArr.length) * 100 + "%";
     },
