f05426750caba66cee0d4da5b8a696281f0948c0
Brian Beam <beam1249@me.com>
2023-02-08 21:05:34 -0500

    tested and finished corse progress function

--- capstone/vue/src/components/StudentProgress.vue
+++ capstone/vue/src/components/StudentProgress.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <p id="calculate-progress-show">
-      Course Progress: {{ calculateProgress }}
-    </p>
-    
+    <p id="calculate-progress-show">Course Progress: {{ calculateProgress }}</p>
   </div>
 </template>
 
@@ -19,17 +16,17 @@ export default {
   computed: {
     calculateProgress() {
       let score = 0;
-      let length=this.progressArr.length;
+      let length = this.progressArr.length;
       if (this.progressArr.length === 0) {
         return 0 + "%";
       }
-      for (let i = 0; i < this.progressArr.length -1; i++) {
-        if(this.progressArr[i].grade === 0){
-         length=length-1;
+      for (let i = 0; i < this.progressArr.length; i++) {
+        if (this.progressArr[i].grade === 0) {
+          length = length - 1;
         }
-        score = this.progressArr[i].grade + score;   
+        score = this.progressArr[i].grade + score;
       }
-      return (score / this.progressArr.length) * 100 + "%";
+      return (score / length) * 10 + "%";
     },
   },
   created() {
